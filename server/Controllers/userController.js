250a7248da5818c1723d58e6e9ec5d4691d43d2e
pratha-m <prathamchhabra85@gmail.com>
2024-01-24 00:31:43 +0530

    fix - fixed cookie issue in production

--- server/Controllers/userController.js
+++ server/Controllers/userController.js
@@ -18,7 +18,7 @@ const createUser=async(req,res,next)=>{
             httpOnly:false,
             secure:true,
             path:"/",
-            domain:"blogii.netlify.app",
+            domain:".blogii.netlify.app",
             withCredentials:true,
             maxAge:3*24*60*60*1000,
             sameSite: 'None'
@@ -47,7 +47,7 @@ const loginUser=async(req,res,next)=>{
             httpOnly:false,
             secure:true,
             path:"/",
-            domain:"blogii.netlify.app",
+            domain:".blogii.netlify.app",
             withCredentials:true,
             maxAge:3*24*60*60*1000,
             sameSite: 'None'
